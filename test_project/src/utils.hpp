eb216a776258d81bd2ba1ec2f62de27ccbe82157
hadrianhu888 <hadrian.hu@gmail.com>
2023-08-14 19:55:27 -0400

    Created battery project Created simple_test to test c/c++ functionality Created project to test c.,cpp integration

---
+++ test_project/src/utils.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file utils.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+void print_hello();
+
+#endif // __UTILS_HPP__
+
